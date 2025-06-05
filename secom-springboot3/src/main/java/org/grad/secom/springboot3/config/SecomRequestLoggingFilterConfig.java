aacc1c211fd73a2d65a29865d5696429fdb8a142
Lawrence Hughes <lawrence.hughes@gla-rad.org>
2025-06-05 09:18:13 +0100

    Changed copyright year to clear some changes in the pull request

--- secom-springboot3/src/main/java/org/grad/secom/springboot3/config/SecomRequestLoggingFilterConfig.java
+++ secom-springboot3/src/main/java/org/grad/secom/springboot3/config/SecomRequestLoggingFilterConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 GLA Research and Development Directorate
+ * Copyright (c) 2022 GLA Research and Development Directorate
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
