f31ec216c1a0a95e7683c202281c0f08596a690f
Lawrence Hughes <lawrence.hughes@gla-rad.org>
2025-06-06 10:28:03 +0100

    Updated copyright

--- secom-core-jakarta/src/main/java/org/grad/secom/core/base/SecomCompressionProvider.java
+++ secom-core-jakarta/src/main/java/org/grad/secom/core/base/SecomCompressionProvider.java
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2022 GLA Research and Development Directorate
+ * Copyright (c) 2025 GLA Research and Development Directorate
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
