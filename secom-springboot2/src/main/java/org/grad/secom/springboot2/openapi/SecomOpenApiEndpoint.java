91bc36bfb214d6a4e976a8ffa649ba504401a5f2
Nikolaos Vastardis <Nikolaos.Vastardis@gla-rad.org>
2025-07-07 12:07:28 +0100

    Adding openapi endpoints in the springboot modules

---
+++ secom-springboot2/src/main/java/org/grad/secom/springboot2/openapi/SecomOpenApiEndpoint.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2025 GLA Research and Development Directorate
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.grad.secom.springboot2.openapi;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.jaxrs2.Reader;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.grad.secom.core.base.SecomConstants;
+import org.grad.secom.core.interfaces.GenericSecomInterface;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * The SECOM OpenAPI Endpoint
+ * <p/>
+ * This is the implementation of an endpoint that provides the OpenAPI
+ * definition for the Springboot service using the SECOMLib. It
+ * automatically identifies all registered controller components and
+ * parses them to generate the OpenAPI JSON file which can be served
+ * through Swagger.
+ *
+ * @author Nikolaos Vastardis (email: Nikolaos.Vastardis@gla-rad.org)
+ */
+@Component
+@Path("/")
+public class SecomOpenApiEndpoint {
+
+    /**
+     * The Application Context.
+     */
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    /**
+     * The SECOM OpenAPI Info Provider Component.
+     */
+    @Autowired(required = false)
+    private SecomOpenApiInfoProvider secomOpenApiInfoProvider;
+
+    /**
+     * GET /v1/openapi.json : returns the OpenAPI JSON file generated
+     * automatically for the controller components extending the
+     * GenericSecomInterface class.
+     *
+     * @return the OpenAPIJson
+     * @throws JsonProcessingException when the JSON processing fails
+     */
+    @Path("/" + SecomConstants.SECOM_VERSION + "/openapi.json")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getOpenApiV1() throws JsonProcessingException {
+        // Get the basic OpenAPI information and create a Reader for scanning
+        final Reader reader = new Reader(Optional.ofNullable(secomOpenApiInfoProvider)
+                .map(SecomOpenApiInfoProvider::getOpenApiInfo)
+                .orElseGet(SecomOpenApiInfoProvider::defaultOpenAPIInfo));
+
+        // Swagger Core Reader will scan these classes for annotations
+        final Set<Class<?>> classes = this.applicationContext.getBeansOfType(GenericSecomInterface.class).values()
+                .stream()
+                .map(AopUtils::getTargetClass)
+                .collect(Collectors.toSet());
+
+        // Now parse through the SECOM interfaces and generate the OpenAPI JSON
+        final ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        return Response.ok(objectMapper.writeValueAsString(reader.read(classes))).build();
+    }
+
+}
---
+++ secom-springboot2/src/main/java/org/grad/secom/springboot2/openapi/SecomOpenApiInfoProvider.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2025 GLA Research and Development Directorate
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.grad.secom.springboot2.openapi;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.servers.Server;
+
+import java.util.Arrays;
+
+/**
+ * The SECOM OpenAPI information Provider Interface
+ * <p/>
+ * A helper interface to provide the basic information required to the
+ * generation of the OpenAPI documentation in the implementing service.
+ *
+ * @author Nikolaos.Vastardis@gla-rad.org
+ */
+public interface SecomOpenApiInfoProvider {
+
+    /**
+     * Returns the OpenAPI definition information used for the service
+     * documentation.
+     *
+     * @return the OpenAPI definition.
+     */
+    OpenAPI getOpenApiInfo();
+
+    /**
+     * Returns the default OpenAPI definition information used for the service
+     * documentation.
+     *
+     * @return the default OpenAPI definition.
+     */
+    static OpenAPI defaultOpenAPIInfo() {
+        return new OpenAPI().schema("secom", new Schema<>().$schema("openapi.json"))
+                .info(new Info().title("SECOM Interfaces")
+                        .description("The SECOM Interfaces of the Service")
+                        .termsOfService("")
+                        .version("v0.0.1")
+                        .contact(new Contact().email("nam@test.org"))
+                        .license(new License().name("Apache 2.0").url("http://www.apache.org/licenses/LICENSE-2.0.html")))
+                .servers(Arrays.asList(new Server[]{
+                        new Server().url("http://localhost:8080/api/secom2")
+                }))
+                .externalDocs(new ExternalDocumentation()
+                        .description("SpringShop Wiki Documentation")
+                        .url("https://springshop.wiki.github.org/docs"));
+    }
+
+}
