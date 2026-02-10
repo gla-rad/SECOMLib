33afd400bf2db54cfa4ac2d795af27dcbef7053e
Nikolaos Vastardis <Nikolaos.Vastardis@gla-rad.org>
2026-02-10 11:12:21 +0000

    Adding a SECOM v2.0 springboot4 package

---
+++ secom-v2-springboot4/src/main/java/org/grad/secomv2/springboot3/config/SecomV2JaxrsApplication.java
@@ -0,0 +1,185 @@
+/*
+ * Copyright (c) 2025 GLA Research and Development Directorate
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.grad.secomv2.springboot3.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.jaxrs2.integration.resources.AcceptHeaderOpenApiResource;
+import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import org.grad.secomv2.core.base.*;
+import org.grad.secomv2.core.components.*;
+import org.jboss.resteasy.plugins.interceptors.CorsFilter;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * JAX-RS application
+ *
+ * @author Nikolaos Vastardis (email: Nikolaos.Vastardis@gla-rad.org)
+ */
+@Configuration
+@ApplicationPath("/api/secom/")
+public class SecomV2JaxrsApplication extends Application implements ApplicationContextAware  {
+
+    /**
+     * The Springboot Application Context.
+     */
+    private ApplicationContext applicationContext;
+
+    /**
+     * Autowiring the Springboot Object Mapper
+     */
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    /**
+     * Initialise the SECOM exception mapper.
+     *
+     * @return the SECOM exception mapper bean
+     */
+    @Bean("secomV2ExceptionMapper")
+    SecomV2ExceptionMapper secomExceptionMapper() {
+        return new SecomV2ExceptionMapper(this);
+    }
+
+    /**
+     * Initialise the SECOM writer interceptor.
+     *
+     * @return the SECOM writer interceptor bean
+     */
+    @Bean("secomv2WriterInterceptor")
+    SecomWriterInterceptor secomWriterInterceptor(@Autowired(required = false) SecomCompressionProvider compressionProvider,
+                                                  @Autowired(required = false) SecomEncryptionProvider encryptionProvider,
+                                                  @Autowired(required = false) SecomCertificateProvider certificateProvider,
+                                                  @Autowired(required = false) SecomSignatureProvider signatureProvider) {
+        return new SecomWriterInterceptor(compressionProvider, encryptionProvider, certificateProvider, signatureProvider);
+    }
+
+    /**
+     * Initialise the SECOM signature filter.
+     *
+     * @return the SECOM signature filter bean
+     */
+    @Bean("secomV2SignatureFilter")
+    SecomSignatureFilter secomSignatureFilter(@Autowired(required = false) SecomCompressionProvider compressionProvider,
+                                              @Autowired(required = false) SecomEncryptionProvider encryptionProvider,
+                                              @Autowired(required = false) SecomTrustStoreProvider trustStoreProvider,
+                                              @Autowired(required = false) SecomSignatureProvider signatureProvider) {
+        return new SecomSignatureFilter(compressionProvider, encryptionProvider, trustStoreProvider, signatureProvider);
+    }
+
+    /**
+     * Initialise the SECOM reader interceptor.
+     *
+     * @return the SECOM reader interceptor bean
+     */
+    @Bean("secomV2ReaderInterceptor")
+    SecomReaderInterceptor secomReaderInterceptor(@Autowired(required = false) SecomCompressionProvider compressionProvider,
+                                                  @Autowired(required = false) SecomEncryptionProvider encryptionProvider) {
+        return new SecomReaderInterceptor(compressionProvider, encryptionProvider);
+    }
+
+    /**
+     * Register the required classes to the RESTEasy server.
+     *
+     * @return the set of classes to be registered.
+     */
+    @Override
+    public Set<Class<?>> getClasses() {
+        return Set.of(
+                OpenApiResource.class,
+                AcceptHeaderOpenApiResource.class,
+                /*
+                 * Add the JaxRS Application Providers.
+                 */
+                InstantToISOConverterProvider.class,
+                ContainerTypeConverterProvider.class,
+                DigitalSignatureAlgorithmConverterProvider.class
+        );
+    }
+
+    /**
+     * Register the required singletons to the RESTEasy server.
+     *
+     * @return the set of singletons to be registered.
+     */
+    @Override
+    public Set<Object> getSingletons() {
+        CorsFilter corsFilter = new CorsFilter();
+        corsFilter.getAllowedOrigins().add("*");
+        corsFilter.setAllowedMethods("OPTIONS, GET, POST, DELETE, PUT, PATCH");
+        corsFilter.setAllowCredentials(false);
+        return Set.of(
+                corsFilter,
+                /*
+                 * Add the JaxRS Application Object Mapper.
+                 */
+                new SecomObjectMapperProvider(Optional.ofNullable(this.objectMapper)
+                        .orElse(new ObjectMapper()))
+        );
+    }
+
+
+    /**
+     * Returns the properties of the JaxRS application. This is actually
+     * also used internally to provide access to all registered RestEasy
+     * Exception mappers.
+     *
+     * @return the set of properties to be registered
+     */
+    @Override
+    public Map<String, Object> getProperties() {
+        return this.applicationContext.getBeansOfType(ExceptionMapper.class)
+                .entrySet()
+                .stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    /**
+     * Allows the retrieval of the Springboot application context.
+     *
+     * @return the Springboot application context
+     */
+    public ApplicationContext getApplicationContext() {
+        return this.applicationContext;
+    }
+
+    /**
+     * Implements the setApplicationContext() function of the Springboot
+     * ApplicationContextAware interface so that the JaxRS application can
+     * have access to the application context.
+     *
+     * @param applicationContext the Springboot application context
+     * @throws BeansException if an exception on the bean generation is thrown
+     */
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+}
---
+++ secom-v2-springboot4/src/main/java/org/grad/secomv2/springboot3/openapi/SecomV2OpenApiEndpoint.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2025 GLA Research and Development Directorate
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.grad.secomv2.springboot3.openapi;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import io.swagger.v3.jaxrs2.Reader;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.grad.secomv2.core.base.SecomConstants;
+import org.grad.secomv2.core.interfaces.GenericSecomInterface;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * The SECOM OpenAPI Endpoint
+ * <p/>
+ * This is the implementation of an endpoint that provides the OpenAPI
+ * definition for the Springboot service using the SECOMLib. It
+ * automatically identifies all registered controller components and
+ * parses them to generate the OpenAPI JSON file which can be served
+ * through Swagger.
+ *
+ * @author Nikolaos Vastardis (email: Nikolaos.Vastardis@gla-rad.org)
+ */
+@Component
+@Path("/")
+public class SecomV2OpenApiEndpoint {
+
+    /**
+     * The Application Context.
+     */
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    /**
+     * The Default ObjectMapper/
+     */
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    /**
+     * The SECOM OpenAPI Info Provider Component.
+     */
+    @Autowired(required = false)
+    private SecomV2OpenApiInfoProvider secomV2OpenApiInfoProvider;
+
+    /**
+     * GET /v2/openapi : returns the OpenAPI JSON file generated automatically
+     * for the controller components extending the GenericSecomInterface class.
+     *
+     * @return the OpenAPIJson
+     * @throws JsonProcessingException when the JSON processing fails
+     */
+    @Path("/" + SecomConstants.SECOM_VERSION + "/openapi.json")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getOpenApiV2() throws JsonProcessingException {
+        // Get the basic OpenAPI information and create a Reader for scanning
+        final Reader reader = new Reader(Optional.ofNullable(secomV2OpenApiInfoProvider)
+                .map(SecomV2OpenApiInfoProvider::getOpenApiInfo)
+                .orElseGet(SecomV2OpenApiInfoProvider::defaultOpenAPIInfo));
+
+        // Swagger Core Reader will scan these classes for annotations
+        final Set<Class<?>> v2Classes = this.applicationContext.getBeansOfType(GenericSecomInterface.class).values()
+                .stream()
+                .map(AopUtils::getTargetClass)
+                .collect(Collectors.toSet());
+
+        // Now parse through the SECOM interfaces and generate the OpenAPI JSON
+        this.objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        this.objectMapper.registerModule(new JavaTimeModule());
+        return Response.ok(this.objectMapper.writeValueAsString(reader.read(v2Classes))).build();
+    }
+
+}
